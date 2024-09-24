bcb0ce81e8015260016302f4c970c9664f79fda7
Garyn14 <garyn.fernando.hr@gmail.com>
2024-09-23 22:50:23 -0500

    improve task model with validation

--- cli/src/main/java/roadmap/tast_tracker/cli/model/Task.java
+++ cli/src/main/java/roadmap/tast_tracker/cli/model/Task.java
@@ -2,7 +2,10 @@ package roadmap.tast_tracker.cli.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import roadmap.tast_tracker.cli.enums.TaskStatus;
 
@@ -12,42 +15,57 @@ import java.time.format.DateTimeFormatter;
 @Entity
 @Table(name = "tasks")
 @Getter @Setter
+@NoArgsConstructor
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
+    @NotBlank(message = "Description cannot be blank")
+    @Size(max = 255, message = "Description must be between 3 and 255 characters")
+    @Column(name = "description")
     private String description;
 
     @Enumerated(EnumType.STRING)
+    @Column(name = "status",  nullable = false)
     private TaskStatus status;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
-    public Task() {
-        this.status = TaskStatus.TO_DO;
-        this.createdAt = LocalDateTime.now();
-        this.updatedAt = LocalDateTime.now();
-    }
+    @Transient
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 
     public Task(String description) {
-        this();
         this.description = description;
     }
 
     @Override
     public String toString() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
         return "\nTask{" +
                 "id=" + id +
                 ", description='" + description + '\'' +
                 ", status=" + status +
-                ", createdAt=" + createdAt.format(formatter) +
-                ", updatedAt=" + updatedAt.format(formatter) +
+                ", createdAt=" + createdAt.format(FORMATTER) +
+                ", updatedAt=" + updatedAt.format(FORMATTER) +
                 "}";
+    }
+
+    @PrePersist
+    public void prePersist() {
+        this.status = TaskStatus.TO_DO;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
     }
 }
--- cli/src/main/resources/db/migration/V1__create-table-tasks.sql
+++ cli/src/main/resources/db/migration/V1__create-table-tasks.sql
@@ -4,7 +4,7 @@ CREATE TYPE task_status AS ENUM ('to-do', 'in-progress', 'done');
 -- create table tasks
 CREATE TABLE tasks (
     id BIGSERIAL PRIMARY KEY,
-    description VARCHAR(255),
+    description VARCHAR(255) NOT NULL,
     status task_status,
     created_at TIMESTAMP,
     updated_at TIMESTAMP
