52788ea2f5f74022bf6d61cde99db73f84027c0c
Garyn14 <garyn.fernando.hr@gmail.com>
2024-09-23 22:06:34 -0500

    change model, add validate to hibernate propertie and create a migration with flyway

--- cli/src/main/java/roadmap/tast_tracker/cli/model/Task.java
+++ cli/src/main/java/roadmap/tast_tracker/cli/model/Task.java
@@ -1,6 +1,7 @@
 package roadmap.tast_tracker.cli.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import roadmap.tast_tracker.cli.enums.TaskStatus;
@@ -8,24 +9,34 @@ import roadmap.tast_tracker.cli.enums.TaskStatus;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-@Setter
-@Getter
+@Entity
+@Table(name = "tasks")
+@Getter @Setter
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private static Long nextId = 0L;
+
     private String description;
+
+    @Enumerated(EnumType.STRING)
     private TaskStatus status;
+
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime createdAt;
+
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime updatedAt;
 
-    public Task(String description) {
-        this.id = ++nextId;
-        this.description = description;
+    public Task() {
         this.status = TaskStatus.TO_DO;
         this.createdAt = LocalDateTime.now();
         this.updatedAt = LocalDateTime.now();
+    }
+
+    public Task(String description) {
+        this();
+        this.description = description;
     }
 
     @Override
---
+++ cli/src/main/resources/db/migration/V1__create-table-tasks.sql
@@ -0,0 +1,11 @@
+-- create ENUM for TaskStatus
+CREATE TYPE task_status AS ENUM ('to-do', 'in-progress', 'done');
+
+-- create table tasks
+CREATE TABLE tasks (
+    id BIGSERIAL PRIMARY KEY,
+    description VARCHAR(255),
+    status task_status,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+);
